bf7a7b966c19816b1cfeaee38dac59e74e67dbfd
Chris Kane <info@chriskanedev.com>
2021-04-11 22:56:38 +0100

    Add 3 unit tests

--- src/notification.py
+++ src/notification.py
@@ -1,4 +1,4 @@
-from DEFINITIONS import *
+from src.DEFINITIONS import *
 
 from win10toast import ToastNotifier
 
@@ -7,3 +7,4 @@ def display_notification():
     toaster = ToastNotifier()
     toaster.show_toast(NOTIFICATION_TITLE, NOTIFICATION_DESCRIPTION, icon_path=NOTIFICATION_ICON_PATH,
                        duration=NOTIFICATION_DURATION)
+    return True
